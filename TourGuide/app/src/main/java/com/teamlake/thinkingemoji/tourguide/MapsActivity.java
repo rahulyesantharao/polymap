67d3d4b9904110e9238191b2c44e2d91fbfe07e1
George Zhang <gzhang1218@gmail.com>
2017-07-15 13:46:35 -0500

    partially completed adjustment of zoom based on polyline

--- TourGuide/app/src/main/java/com/teamlake/thinkingemoji/tourguide/MapsActivity.java
+++ TourGuide/app/src/main/java/com/teamlake/thinkingemoji/tourguide/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.teamlake.thinkingemoji.tourguide;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -8,9 +9,17 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity
+        implements
+            OnMapReadyCallback{
 
     private GoogleMap mMap;
 
@@ -38,9 +47,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //List<LatLng> points = ;
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        //   for (LatLng item : points) {
+        //    builder.include(item);
+       //    }
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //LatLng houston = new LatLng(29.76,-93.37);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.addMarker(new MarkerOptions().position(houston).title("Marker in Houston"));
+        //Polyline polyline1 = mMap.addPolyline(new PolylineOptions().width(11).startCap(new RoundCap()).endCap(new RoundCap()).color(Color.RED).add(sydney, houston));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 0));
     }
 }
