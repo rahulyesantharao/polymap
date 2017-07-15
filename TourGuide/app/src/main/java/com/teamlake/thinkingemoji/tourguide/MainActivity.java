48ed74d07263c529940e86201c7f5ff80f34eff9
Daniel Ho <danielho@cs.utexas.edu>
2017-07-15 03:07:04 -0500

    Started location feature

--- TourGuide/app/src/main/java/com/teamlake/thinkingemoji/tourguide/MainActivity.java
+++ TourGuide/app/src/main/java/com/teamlake/thinkingemoji/tourguide/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    private FusedLocationProviderClient mFusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
