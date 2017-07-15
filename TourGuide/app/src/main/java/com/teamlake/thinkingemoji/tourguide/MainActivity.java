4a0da0f1ed391123763a0b7805d6436e2487c246
George Zhang <gzhang1218@gmail.com>
2017-07-15 03:48:14 -0500

    created map button and map with marker

--- TourGuide/app/src/main/java/com/teamlake/thinkingemoji/tourguide/MainActivity.java
+++ TourGuide/app/src/main/java/com/teamlake/thinkingemoji/tourguide/MainActivity.java
@@ -1,7 +1,11 @@
 package com.teamlake.thinkingemoji.tourguide;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.net.Uri;
 
 public class MainActivity extends Activity {
 
@@ -14,5 +18,16 @@ public class MainActivity extends Activity {
                     .replace(R.id.container, Camera2BasicFragment.newInstance())
                     .commit();
         }
+    }
+    public void switchMap(View view) {
+        // Create a Uri from an intent string. Use the result to create an Intent.
+        Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194?z=0");
+        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(MainActivity.this, MapsActivity.class);
+        // Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+        // Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+        Log.d("meme","button pressed");
     }
 }
